dee8fc9cd72f8127ddfe6040bb189c96a2f648ec
TheBigL <lmohamed1@studentmail.nait.ca>
2018-04-01 20:23:09 -0600

    Setting up the Karaoke Machine Test

--- tests/com/mohamed/leban/model/KaraokeMachineTest.java
+++ tests/com/mohamed/leban/model/KaraokeMachineTest.java
@@ -1,9 +1,37 @@
 package com.mohamed.leban.model;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import javax.annotation.processing.Generated;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class KaraokeMachineTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class KaraokeMachineTest
 {
+    private SongBook songBook;
+    private BufferedReader bufferedReader;
+    private HashMap<String, String> menu;
+    KaraokeMachine karaokeMachine;
+
+
+
+    @BeforeAll
+    public void setUp()
+    {
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        menu = new HashMap<>();
+        songBook = new SongBook();
+        karaokeMachine = new KaraokeMachine(songBook);
+
+    }
+
 
 
 }
